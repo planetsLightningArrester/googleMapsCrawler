7f57efc105a84e3a4d501a81cc0a6dffd85b663e
Planet's Lightning Arrester <franciscogssm94@gmail.com>
2023-12-27 22:40:25 -0300

    fix crawler by using `$x`

--- index.js
+++ index.js
@@ -4,41 +4,32 @@ let browser;
 let page;
 
 async function main() {
-  //Open the browser
-  browser = await puppeteer.launch();
+  // Open the browser
+  browser = await puppeteer.launch({ headless: true });
   page = await browser.newPage();
 
-  //Get the distance
+  // Get the distance
   let distance, time;
-  let origem = [
+  let from = [
     'Belo Horizonte',
     'Belo Horizonte',
     'Juiz de Fora',
     'Ouro Branco',
     'Congonhas'
   ],
-  destino = [
-    'Juiz de Fora',
-    'Uberlandia',
-    'Rio de Janeiro',
-    'Belo Horizonte',
-    'Belo Horizonte'
-  ];
+    to = [
+      'Juiz de Fora',
+      'Uberlandia',
+      'Rio de Janeiro',
+      'Belo Horizonte',
+      'Belo Horizonte'
+    ];
 
-  for (let i = 0; i < origem.length; i++) {
-    [distance, time] = await getDistanceAndTime(origem[i], destino[i]);
-    console.log('Distância entre ' + origem[i] + ' e ' + destino[i] + ': ' + distance + '.\nPrevisão de tempo de viagem: ' + time + '.');
+  for (let i = 0; i < from.length; i++) {
+    [distance, time] = await getDistanceAndTime(from[i], to[i]);
+    console.log('Distance between ' + from[i] + ' and ' + to[i] + ': ' + distance + '.\nTravel duration estimative: ' + time + '.');
   }
-  
-  // origem = "Belo Horizonte";
-  // destino = "Juiz de Fora";
-  // [distance, time] = await getDistanceAndTime(origem, destino);
-  // console.log('Distância entre BH e JF: ' + distance + '.\n Previsão de tempo de viagem: ' + time + '.');
 
-  // origem = "Belo Horizonte";
-  // destino = "Uberlandia";
-  // [distance, time] = await getDistanceAndTime(origem, destino);
-  // console.log('Distância entre BH e JF: ' + distance + '.\n Previsão de tempo de viagem: ' + time + '.');
   await browser.close();
 
 }
@@ -48,12 +39,11 @@ async function getDistanceAndTime(to, from) {
   let url = 'https://www.google.com/maps/dir/' + to.split(' ').join('+') + '/' + from.split(' ').join('+');
 
   await page.goto(url);
-  let distance = await page.$$eval('#section-directions-trip-0 > div.section-directions-trip-description > div:nth-child(1) > div.section-directions-trip-numbers > div.section-directions-trip-distance.section-directions-trip-secondary-text > div', element => element.map(el => el.textContent));
-  let time = await page.$$eval('#section-directions-trip-0 > div.section-directions-trip-description > div:nth-child(1) > div.section-directions-trip-numbers > div.section-directions-trip-duration.delay-light > span:nth-child(1)', element => element.map(el => el.textContent));
-  
-  return [distance[0], time[0]];
+  let distance = (await page.$x("//div[text()[contains(.,'km')]]"))[0];
+  let parent = (await (await distance.getProperty("parentNode")).getProperty("parentNode"));
+  let time = (await parent.$$("div"))[0];
+
+  return [await page.evaluate(el => el.innerText, distance), await page.evaluate(el => el.innerText, time)];
 };
 
 main();
-
-// #section-directions-trip-0 > div.section-directions-trip-description > div:nth-child(1) > div.section-directions-trip-numbers > div.section-directions-trip-duration.delay-light > span:nth-child(1)
